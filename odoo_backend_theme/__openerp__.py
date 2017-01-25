caead1ac44dc5c9931b7512bc0828c4aecc4efba
odoo17 <odoo90@outlook.com>
2017-01-25 12:43:32 +0200

    add

---
+++ odoo_backend_theme/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'digital back-end theme',
+    'version' : '0.1',
+    'author' : 'Developer',
+    'category' : 'Tools',
+    'website' : '',
+    'description': """
+Give Odoo 8.0 amazing Style.
+    """,
+    'depends' : ['base'],
+	'price' : 100,
+	'currency' : 'EUR',
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
